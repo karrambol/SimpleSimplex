ec4d707a578d52946132f1cb97f1cab4f2f7f626
karrambol <xaunt92@gmail.com>
2019-10-05 04:21:30 +0500

    Init

---
+++ test/test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var NMSimplex = require('../src/NMSimplex.js');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Old-style args', function() {
+    describe('Resuld', function() {
+      it('should return correct ', function() {
+        
+
+
+
+
+        assert.equal([1, 2, 3].indexOf(4), -1);
+      });
+    });
+  });
